0bda126a86f14e86bf7769aa161b7de7481ed79e
gysagsohn <gysagsohn@hotmail.com>
2023-12-09 11:31:49 +1100

    view todo

--- todo_list/todo_functions.py
+++ todo_list/todo_functions.py
@@ -6,9 +6,9 @@ def add_todo(file_name):
     todo_name = input("Enter a todo: ")
     # Open file - list.csv
     with open(file_name, "a") as f:
-    # while inserting - title = user entered
-                    # - completed = False
         writer = csv.writer(f)
+        # Insert values - title = user entered
+                      # - completed = False
         writer.writerow([todo_name, "False"])
 
 def remove_todo(file_name):
@@ -19,3 +19,11 @@ def mark_todo(file_name):
 
 def view_todo(file_name):
     print("View todo")
+    with open(file_name, "r") as f:
+        reader = csv.reader(f)
+        reader._next_()
+        for row in reader:
+            if (row[1] == "True"):
+                print(f"Todo {row[0]} is complete")
+            else:
+                print(f"Todo {row[0]} is not complete")
